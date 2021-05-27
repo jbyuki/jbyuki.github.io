6dc8dfba9dc17b961ad93089c4ddee5c7bedc941
jbyuki <jyburkhard@hotmail.com>
2021-05-27 20:38:17 +0200

    dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 const togglemode = document.querySelector('#togglemode')
 togglemode.addEventListener('click', function() {
   document.body.classList.toggle('darkmode');
+  sessionStorage.setItem('darkmode', togglemode.checked);
 });
+
+var darkmode = sessionStorage.getItem('darkmode');
+console.log(darkmode);
+
+if(darkmode === null) {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    darkmode = 'true';
+  }
+}
+
+if(darkmode === "true") {
+  togglemode.checked = true;
+  document.body.classList.toggle('darkmode');
+}
+
